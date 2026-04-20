d8e41a9028680f87a735c4fe68e28dc02811f6ed
nguyenhuy <147140892+melodyyy111@users.noreply.github.com>
2026-04-21 00:37:53 +0700

    Update DBContext.java

--- src/main/java/com/mycompany/ltw/utils/DBContext.java
+++ src/main/java/com/mycompany/ltw/utils/DBContext.java
@@ -2,17 +2,23 @@ package com.mycompany.ltw.utils;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class DBContext {
     
-    private final String serverName = "localhost";
-    private final String dbName = "hotel_booking_db";
-    private final String portNumber = "3306";
-    private final String userID = "root";
-    private final String password = "241105"; // Thay pass vào đây
 
-    public Connection getConnection() throws Exception {
+    private static final String serverName = "localhost";
+    private static final String dbName = "hotel_booking_db";
+    private static final String portNumber = "3306";
+    private static final String userID = "root";
+    private static final String password = "241105"; // Thay pass vào đây
+
+
+
+    public DBContext() {
+    }
+    
+    public static Connection getConnection() throws Exception {
+
         String url = "jdbc:mysql://" + serverName + ":" + portNumber + "/" + dbName;
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(url, userID, password);
@@ -21,7 +27,6 @@ public class DBContext {
     // Test kết nối
     public static void main(String[] args) {
         try {
-            System.out.println(new DBContext().getConnection());
             System.out.println("Kết nối thành công!");
         } catch (Exception e) {
             e.printStackTrace();
